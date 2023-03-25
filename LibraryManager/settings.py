2a9dee3580816e4387ec4c9111ab0d4a58640253
susreboot <joshbrade@gmail.com>
2023-03-25 17:55:05 -0400

    changed settings to remove decouple

--- LibraryManager/settings.py
+++ LibraryManager/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-from decouple import config
+# from decouple import config
 import os
 import django_heroku
 # from decouple import config
